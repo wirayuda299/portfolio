8e3ee4bc89a50240a2dfc20d4ca8178aaea3f414
wirayuda <wirayuda233@gmail.com>
2023-09-10 22:12:57 +0800

    remove margin in cta banner

--- components/CallToAction/CallToAction.tsx
+++ components/CallToAction/CallToAction.tsx
@@ -3,7 +3,7 @@ import { Button } from '../ui/button';
 
 export default function CallToAction() {
 	return (
-		<section className='mb-10 mt-24 flex h-full min-h-[500px] w-full items-center p-3 dark:bg-black-200 sm:p-6'>
+		<section className=' flex h-full min-h-[500px] w-full items-center p-3 dark:bg-black-200 sm:p-6'>
 			<div className="mx-auto flex h-full w-full max-w-[1400px] flex-col  items-center justify-center rounded-xl  bg-[url('/assets/images/cta.png')] bg-auto bg-center bg-no-repeat p-4 sm:aspect-video sm:h-[330px] sm:bg-cover sm:p-10 md:flex-row md:justify-between lg:p-12">
 				<div className='max-w-xl'>
 					<h2 className='p-5 text-3xl font-bold sm:text-center md:text-left md:text-[32px] lg:text-5xl'>
